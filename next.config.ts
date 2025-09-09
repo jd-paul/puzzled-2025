fc25a7944a1638ce7eb9147c3a322528de5d46d8
Paul San Diego <johnpaul20030@gmail.com>
2025-09-09 16:34:54 +0100

    added one line to next.config.ts for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",          // produce static files in /out
+  output: "export",           // produce static files in /out
   images: { unoptimized: true },
-  // No basePath/assetPrefix because you use a custom domain
+                              // No basePath/assetPrefix because you use a custom domain
+  trailingSlash: true,        // helps with cPanel directory indexing
 };
 
 export default nextConfig;
