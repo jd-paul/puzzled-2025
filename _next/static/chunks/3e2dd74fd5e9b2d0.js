8135db3618f84dbcce50f957a4f342c2b749841c
Paul San Diego <johnpaul20030@gmail.com>
2025-09-09 15:52:32 +0100

    Publish static site

---
+++ _next/static/chunks/3e2dd74fd5e9b2d0.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/b5d67e5b12b40280.js",
+  "static/chunks/105dea3b1ca71b32.js",
+  "static/chunks/turbopack-6992c9e9e7b1c191.js"
+])
---
+++ _next/static/chunks/a0900934391ae839.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/a7ae3ec2489c2858.js",
+  "static/chunks/105dea3b1ca71b32.js",
+  "static/chunks/turbopack-b64499f6e3475e9c.js"
+])
